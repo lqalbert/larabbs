114e697d1f2168221d9d85825d648fbe2ff8ab8a
liuqing <liuq@riign.com>
2018-07-13 11:05:33 +0800

    finish user auth

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,26 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+//laravel的用户认证路由（登录，注册，密码重置），php artisan make:auth自动生成
+Auth::routes();
+//等同于如下路由集合
+// Authentication Routes...
+/*Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+*/
+
+
+//网站主页，由于自定义设置了主页，不需要默认生成的，删除此条路由
+//Route::get('/home', 'HomeController@index')->name('home');
