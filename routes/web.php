7d9f37a403f25100e5d8eb42ed9601a211a7c5a4
liuqing <liuq@riign.com>
2018-07-16 09:42:02 +0800

    user can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,5 @@ Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 //分类路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+//上传图片路由
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
