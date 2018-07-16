6c1e8433a14d62b30065f433e7084af7319774c2
liuqing <liuq@riign.com>
2018-07-16 15:34:48 +0800

    notify author when topic replied

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //上传图片路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+//通知路由
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
